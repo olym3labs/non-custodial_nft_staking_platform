8dfaf3b958cdcf320eb42a526ce5817b7548280e
OLYM3 LABS <166474857+olym3labs@users.noreply.github.com>
2024-06-01 14:57:17 +0700

    Update NFT721.sol

--- contracts/NFT721.sol
+++ contracts/NFT721.sol
@@ -1,1 +1,18 @@
 #NFT Collection
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts@5.0.2/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
+
+contract TunaDriverNFT is ERC721, Ownable {
+    constructor(address initialOwner)
+        ERC721("Tuna Driver NFT", "TUNAN")
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+}
