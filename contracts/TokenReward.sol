0dcf08faf361fb72c5be3d7030563267a575f693
OLYM3 LABS <166474857+olym3labs@users.noreply.github.com>
2024-06-04 15:57:05 +0700

    Update TokenReward.sol

--- contracts/TokenReward.sol
+++ contracts/TokenReward.sol
@@ -1,4 +1,4 @@
-#Token Reward on Baobap:  0x8F174e51714108B72B789C86e8d55eafe71c0B4F
+//Token Reward on Baobap:  0x8F174e51714108B72B789C86e8d55eafe71c0B4F
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
